a067e65efbba745bd2de7f9d91ea20df81009055
denischagin <denischaginnn@gmail.com>
2022-01-23 14:46:21 +0300

    Решение квадратного уравнения

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+if __name__ == "__main__":
+    a = float(input('Введите коэф. а:'))
+    b = float(input('Введите коэф. b:'))
+    c = float(input('Введите коэф. c:'))
+    d = b**2-4*a*c
+    if d >= 0:
+        if d == 0:
+            print('Корень квадратного уравнения:\n', -b/2*a)
+        else:
+            print('Корни уравнения:\n', (-b-math.sqrt(d))/2*a, '\n', (-b+math.sqrt(d))/2*a)
+    else:
+        print('Нет корней уравнения')
